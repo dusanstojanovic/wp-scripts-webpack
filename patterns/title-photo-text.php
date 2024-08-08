b8b1b2763e67f1ee8ceb84b2b90127e3c08ae4ff
Dusan Stojanovic <stdusan@gmail.com>
2024-08-08 13:47:21 +0200

    Add empty acf-json folder

---
+++ patterns/title-photo-text.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Title: Title with photo and text
+ * Slug: Title-with-photo-and-text
+ * Categories: themename-patterns
+ * Keywords: boxes, content
+ */
+?>
+
+<!-- wp:group {"tagName":"section","align":"full","backgroundColor":"light-gray","className":"c-section","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull c-section has-light-gray-background-color has-background"><!-- wp:group {"tagName":"section","align":"full","className":"o-container","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull o-container"><!-- wp:heading {"className":"c-txt\u002d\u002dh2 c-txt\u002d\u002dup c-section__title js-stagger-appear"} -->
+<h2 class="wp-block-heading c-txt--h2 c-txt--up c-section__title js-stagger-appear">UNSERE LÖSUNG</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"align":"full","className":"c-grid  c-grid\u002d\u002d1-1"} -->
+<div class="wp-block-columns alignfull c-grid  c-grid--1-1"><!-- wp:column {"className":" u-mrg\u002d\u002dreset  c-txt\u002d\u002dbody-lg  js-stagger-appear"} -->
+<div class="wp-block-column  u-mrg--reset c-txt--body-lg js-stagger-appear"><!-- wp:image {"id":4201,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url(home_url('/')); ?>/wp-content/uploads/2024/05/Bilder-fuer-Landingpage-24-1.svg" alt="" class="wp-image-4201"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"className":"u-mrg\u002d\u002dreset  c-txt\u002d\u002dbody-lg js-stagger-appear"} -->
+<div class="wp-block-column u-mrg--reset  c-txt--body-lg js-stagger-appear"><!-- wp:paragraph -->
+<p>Mit diesem Projekt haben 3D AG und Inacta eine Plattform entwickelt, die den Prozess von der Etikettenproduktion, über die Hersteller, bis zum Endnutzer abdeckt. Die Plattform ermöglicht:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Integrierter Produktionsprozess​</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Die Ersteller bestimmen den Inhalt der Landingpages​</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Zusätzlicher Mehrwert zu den Hologrammen</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>“Greenfield“-Entwicklung mit iterativer Vorgehensweise</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group --></section>
+<!-- /wp:group -->
